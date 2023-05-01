fb378e2e9b9e18484f50bda839fa6387c49b5064
Eugene Jang <105053478+jinchiim@users.noreply.github.com>
2023-05-01 23:48:59 +0900

    [Bronze II] Title: OX퀴즈, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -1,5 +1,4 @@
 n = int(input())
 for _ in range(n):
     quiz = input().split('X')
-    quiza = [(len(i)*(len(i)+1)//2) for i in quiz]
-    print(sum(quiza))
+    print(sum([(len(i)*(len(i)+1)//2) for i in quiz]))
